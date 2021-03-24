c3faf2f835b43169f1dab1a6724a637b6f4622c4
kdurant <wangjun850725@163.com>
2021-03-24 16:24:15 +0800

    chore: a little

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -822,14 +822,13 @@ void MainWindow::initSignalSlot()
 
 void MainWindow::setToolBar()
 {
-    QAction *act[5];
-    act[0] = new QAction("激光器设置", this);
-    act[1] = new QAction("电机设置", this);
-    act[2] = new QAction("高压设置", this);
-    act[3] = new QAction("预览设置", this);
-    act[4] = new QAction("保存文件设置", this);
+    QVector<QAction *> act;
+    act.append(new QAction("激光器设置", this));
+    act.append(new QAction("电机设置", this));
+    act.append(new QAction("高压设置", this));
+    act.append(new QAction("预览设置", this));
     ui->mainToolBar->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < act.size(); i++)
     {
         ui->mainToolBar->addAction(act[i]);
     }
@@ -838,7 +837,6 @@ void MainWindow::setToolBar()
     connect(act[1], &QAction::triggered, this, [this]() { ui->tabWidget->setCurrentIndex(1); });
     connect(act[2], &QAction::triggered, this, [this]() { ui->tabWidget->setCurrentIndex(2); });
     connect(act[3], &QAction::triggered, this, [this]() { ui->tabWidget->setCurrentIndex(3); });
-    connect(act[4], &QAction::triggered, this, [this]() { ui->tabWidget->setCurrentIndex(3); });
 }
 
 void MainWindow::plotSettings()
