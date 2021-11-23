8b36080da3d0ce1e8775dd159e822308f6e92cdb
kdurant <wangjun850725@163.com>
2021-11-23 10:39:03 +0800

    opt: swap angular.y to angular.z

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1506,8 +1506,8 @@ void MainWindow::initSignalSlot()
         itemList.first()->child(1)->child(3)->setText(1, QString::number(angularVelocity.temp, 'g', 6));
 
         itemList.first()->child(2)->child(0)->setText(1, QString::number(angular.x, 'g', 6));
-        itemList.first()->child(2)->child(1)->setText(1, QString::number(angular.y, 'g', 6));
-        itemList.first()->child(2)->child(2)->setText(1, QString::number(angular.z, 'g', 6));
+        itemList.first()->child(2)->child(1)->setText(1, QString::number(angular.z, 'g', 6));
+        itemList.first()->child(2)->child(2)->setText(1, QString::number(angular.y, 'g', 6));
         itemList.first()->child(2)->child(3)->setText(1, QString::number(angular.temp, 'g', 6));
 
         itemList.first()->child(3)->child(0)->setText(1, QString::number(magneticField.x, 'g', 6));
