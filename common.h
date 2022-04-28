7314169fe5071bc1f0b0972301e63c6c4774d6f6
kdurant <wangjun850725@163.com>
2022-04-28 10:59:16 +0800

    chore: 删除一些不使用的代码

--- common.h
+++ common.h
@@ -391,6 +391,22 @@ public:
         }
         return index;
     }
+
+    static QFileInfoList getFileList(QString &path)
+    {
+        QDir          dir(path);
+        QFileInfoList file_list   = dir.entryInfoList(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+        QFileInfoList folder_list = dir.entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot);
+
+        for(int i = 0; i != folder_list.size(); i++)
+        {
+            QString       name            = folder_list.at(i).absoluteFilePath();
+            QFileInfoList child_file_list = getFileList(name);
+            file_list.append(child_file_list);
+        }
+
+        return file_list;
+    }
 };
 
 #endif  // COMMON_H
--- src/navigation/navigation.h
+++ src/navigation/navigation.h
@@ -6,6 +6,8 @@
 #include "bsp_config.h"
 #include "common.h"
 #include <QtCore>
+#include <QFileDialog>
+#include <QMessageBox>
 
 namespace Ui
 {
@@ -17,47 +19,11 @@ class Navigation : public QWidget
     Q_OBJECT
 
 public:
-    struct Point
-    {
-        double longitude;  // 经度
-        double latitude;   // 纬度
-    };
-    struct Line
-    {
-        int number;
-
-        // Ax + By + C = 0
-        double A;
-        double B;
-        double C;
-
-        double          angle;
-        Point           start;
-        Point           end;
-        QVector<double> key;    // x
-        QVector<double> value;  // y
-    };
-
-    struct CurrentTracker
-    {
-        QVector<double> key;    // x
-        QVector<double> value;  // y
-    };
-
     explicit Navigation(QWidget *parent = 0);
     ~Navigation();
 
-    void   initUI();
-    void   initSignalSlot(void);
-    void   plotSettings();
-    void   addTrackerGraph(void);  // 显示实时轨迹
-    void   addTargetGraph(void);   // 显示当前为止与目标的连线
-    int    paserTracker(const QString &filepath);
-    int    setPostion(const BspConfig::Gps_Info &gps);
-    double distanceBetweenPoints(double longitude1, double latitude1, double longitude2, double latitude2);
-    double distanceBetwwenPointsAndLine(double longitude, double latitude);
-
-    double scanWidth(double height, double angle = 15);
+    void initUI();
+    void initSignalSlot(void);
 
     void showGpsInfo(const BspConfig::Gps_Info &gps);
     void showSystemInfo(double speed);
@@ -66,12 +32,10 @@ public:
 private:
     Ui::Navigation *ui;
 
-    QVector<Line>       targetGraph;  // 航线信息
-    Line                historyTracker;
-    CurrentTracker      realTimeTracker;
     BspConfig::Gps_Info prevGpsInfo;
-    bool                hasTrackerGraph{false};
     double              currentSpeed{0};
+
+    QString mapPath;
 };
 
 #endif  // NOTEINFO_H
