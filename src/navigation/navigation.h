823c77b19dad03987537667755b7ff090868ae23
kdurant <wangjun850725@163.com>
2022-07-12 16:01:56 +0800

    refactor: 将轨迹相关操作放到一个文件里

--- src/navigation/navigation.h
+++ src/navigation/navigation.h
@@ -10,6 +10,7 @@
 #include <QMessageBox>
 #include "MapView.h"
 #include "LineWithArrow.h"
+#include "AirArea.h"
 
 namespace Ui
 {
@@ -19,7 +20,6 @@ class Navigation;
 class Navigation : public QWidget
 {
     Q_OBJECT
-
 public:
     explicit Navigation(QWidget *parent = 0);
     ~Navigation();
@@ -27,68 +27,23 @@ public:
     void initUI();
     void initSignalSlot(void);
 
-    /**
-     * @brief 遍历规划航迹之内所有点，判断当前的GPS位置是否在
-     * @param 当前GPS位置的半径，模拟GPS高度
-     * @return
-     */
-    int isPosInDesigned(double r);
-
-    /**
-     * @brief 返回当前测区覆盖率
-     * @return
-     */
-    double checkCoveragePercent();
-
-    int getTestData(void);
-
-    /**
-     * @brief 从航迹规划文件中找到所有航迹的起点和终点
-     *
-     * @param path
-     * @param track
-     */
-    void parseTrackerFile(QString &path, QVector<QPointF> &track);
-
-    /**
-     * @brief 将规划的轨迹分割成点的集合，计算测区覆盖率使用
-     *
-     * @param track
-     * @param nums 每条航线将会被分割成的点数
-     * @param point
-     *
-     * @return
-     */
-    bool splitTracker(QVector<QPointF> &track, int nums, QVector<QPointF> &point);
-
     void updateGpsInfo(BspConfig::Gps_Info &data);
     void showGpsInfo(const BspConfig::Gps_Info &gps);
     void showSystemInfo(double speed);
 
-    double calcSpeed(BspConfig::Gps_Info prev, BspConfig::Gps_Info cur);
-
 protected:
     void timerEvent(QTimerEvent *event);
-
-signals:
-    void receivedGpsInfo();
 
 private:
     Ui::Navigation *ui;
     qint32          timer1s;
 
-    double currentSpeed{0};
-
-    BspConfig::Gps_Info m_currentPos{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    QString             mapPath;
-    int                 m_tile_X_offset{0};
-    int                 m_tile_Y_offset{0};
+    QString mapPath;
+    int     m_tile_X_offset{0};
+    int     m_tile_Y_offset{0};
 
     QVector<QPointF> m_realtime_path;
-    QVector<QPointF> m_designed_path;
-
-    QVector<int>     m_coverage;  // QVector的长度和规划航线划分的点个数一致
-    QVector<QPointF> m_split_tracker;
+    AirArea          m_designedAirArea;
 
     bool isLoadedMap{false};
     bool isLoadedTracker{false};
