5ac53dcf31c02173586b826fd7d42ae988d0d33e
kdurant <wangjun850725@163.com>
2022-11-11 15:25:57 +0800

    fix: 上个提交的一些错误

--- benchmark/bench_drawLineWithAngle.cpp
+++ benchmark/bench_drawLineWithAngle.cpp
@@ -1,7 +1,47 @@
 #include <benchmark/benchmark.h>
 #include <algorithm>
 #include <QtCore>
-#include <QPolygonF>
+#include <QtGui>
+// #include <QPolygonF>
+
+#define pi 3.1415926
+int data2rgb(int data, int *r, int *g, int *b)
+{
+    // step 1
+    //        double ad = 0;
+    //        if(data < 800)
+    //            ad = 0.8 * (data / 800);
+    //        else
+    //            ad = 0.8 + 0.2 * (data - 800) / 3295;
+
+    int f = ((data - 0.0) / (1024 - 0)) * 255;
+
+    if(f >= 0 && f <= 63)
+    {
+        *r = 0;
+        *g = 254 - 4 * f;
+        *b = 255;
+    }
+    else if(f >= 64 && f <= 127)
+    {
+        *r = 0;
+        *g = 4 * f - 254;
+        *b = 510 - 4 * f;
+    }
+    else if(f >= 128 && f <= 191)
+    {
+        *r = 4 * f - 510;
+        *g = 255;
+        *b = 0;
+    }
+    else if(f >= 192 && f <= 255)
+    {
+        *r = 255;
+        *g = 1022 - 4 * f;
+        *b = 0;
+    }
+    return 0;
+}
 
 void drawLineWithAngle(QImage *img, const QVector<double> &data, double angle)
 {
@@ -30,7 +70,7 @@ void drawLineWithAngle(QImage *img, const QVector<double> &data, double angle)
 
 static void bench_drawLineWithAngle(benchmark::State &state)
 {
-    int             image_size{400};
+    int             img_size{400};
     QImage         *image = new QImage(img_size, (img_size >> 2) * 3, QImage::Format_RGB32);
     QVector<double> data;
     data.resize(400);
@@ -44,12 +84,11 @@ static void bench_drawLineWithAngle(benchmark::State &state)
     {
         drawLineWithAngle(image, data, 33);
     }
-
-    BENCHMARK(bench_drawLineWithAngle)
-        ->Unit(benchmark::kMillisecond)
-        ->Arg(5)
-        ->Arg(10)
-        ->Arg(20);
 }
+
+BENCHMARK(bench_drawLineWithAngle)
+    ->Unit(benchmark::kMicrosecond)
+    ->Arg(10000)
+    ->Arg(20000);
 
 BENCHMARK_MAIN();
