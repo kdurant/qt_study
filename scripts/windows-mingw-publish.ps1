dcd64f1b5a3302670f123432cd199400f5e3672b
work <wangjun850725@163.com>
2023-04-25 14:43:17 +0800

    *.exe

--- scripts/windows-mingw-publish.ps1
+++ scripts/windows-mingw-publish.ps1
@@ -17,9 +17,10 @@ function Main() {
 
     New-Item -ItemType Directory $archiveName
     # 拷贝exe
-    Copy-Item bin\$targetName $archiveName\
+    # Copy-Item bin\$targetName $archiveName\
+    Copy-Item bin\*.exe $archiveName\
     # 拷贝依赖
-    windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
+    # windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
     # 删除不必要的文件
     $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
     Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
